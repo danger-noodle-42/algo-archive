43d967a075f2a77fccd4db7dc91634477ecc0b06
bri-engineer <brian.kelly.hwang@gmail.com>
2023-08-19 11:30:54 -0400

    front end boilder plate

---
+++ client/App.js
@@ -0,0 +1,14 @@
+import CodeViewer from './components/CodeViewer';
+import QuestionList from './components/QuestionList';
+
+function App() {
+  return (
+    <div>
+      <CodeViewer value="tbu"/>
+      <QuestionList value="tbu"/>
+    </div>
+  )
+}
+
+
+export default App;
