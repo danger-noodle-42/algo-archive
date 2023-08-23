71c0bcaf4518bd68878702ab8354fa4b45f25c87
Gabriela Passentino <gpassentino@gmail.com>
2023-08-23 14:05:06 -0500

    middleware readProblemTitles is updated to read either all titles or a tag

--- database/problems/readProblemTitles.js
+++ database/problems/readProblemTitles.js
@@ -1,23 +1,28 @@
-const db = require('../model')
+const db = require('../model');
 
 //Expects username
 
-//Outputs data in res.locals.successful, and 
+//Outputs data in res.locals.successful, and
 //res.locals.problemTitles of format [name1, name2, ...]
 const readProblemTitles = async (req, res, next) => {
-  const { username } = req.body;
+  const { username, tag } = req.body;
   try {
-    const queryString = `
-    SELECT title from problems WHERE username=$1;
-    `;
-    const values = [username]
+    const queryString = `SELECT title FROM problems WHERE username=$1;`; //the string that SQL is reading to get data
+    const values = [username]; //values that will be passed into query
+    if (tag) {
+      queryString += ' AND tag = $2'; //concatenating the queryStr if a tag
+      values.push(tag); //add the tag to the values array
+    }
+
     const results = await db.query(queryString, values);
-    res.locals.problemTitles = results.rows.map(e=>e.title).map(e=>e.trim());
+
+    res.locals.problemTitles = results.rows
+      .map((e) => e.title)
+      .map((e) => e.trim()); 
     res.locals.successsful = true;
 
     return next();
-  }
-  catch (e){
+  } catch (e) {
     res.locals.successsful = false;
     return next();
   }
