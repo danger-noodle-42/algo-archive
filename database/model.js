cfcecf03198357f6dfbdda7922f8a5a81bbdb852
Gabriela Passentino <gpassentino@gmail.com>
2023-08-22 19:00:14 -0500

    database is up and running

--- database/model.js
+++ database/model.js
@@ -1,12 +1,12 @@
-const { Pool } = require('pg');
-const { dbConnection } = require('./sensitive');
+const { Pool } = require('pg'); //Pool class is used to manage connections to the database
+const { dbConnection } = require('./sensitive'); //Imports the db connection
 
 const pool = new Pool({
-  connectionString: dbConnection
-});
+  connectionString: dbConnection,
+}); //a new Pool instance using the connection string in the dbConnection object.
 
 module.exports = {
   query: (text, params, callback) => {
-    return pool.query(text, params, callback);
-  }
-};
+    return pool.query(text, params, callback); //invokes query method of the pool object (the db connection pool)
+  },
+}; //exports a query obj with
--- database/sensitive.js
+++ database/sensitive.js
@@ -1,5 +1,14 @@
-const dbConnection = 'postgres://khnlzpcv:XAmRONcUAIo6c7IaIL2oAUtVmw1p6xCL@ruby.db.elephantsql.com/khnlzpcv';
+//GABBY'S DATABASE
+const dbConnection =
+  'postgres://wxzetwxn:U-5d0m822cO8SZq0f6KEPFjhZ_-IeEKi@silly.db.elephantsql.com/wxzetwxn';
 
 module.exports = {
   dbConnection,
-}
+};
+
+//RYAN'S DATABASE
+// const dbConnection = 'postgres://khnlzpcv:XAmRONcUAIo6c7IaIL2oAUtVmw1p6xCL@ruby.db.elephantsql.com/khnlzpcv';
+
+// module.exports = {
+//   dbConnection,
+// }
