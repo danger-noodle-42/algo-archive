553b0c1b0fc5f54b7e7acc57d911002b21d5797e
Carolina Bonitatis <carolina@bonitat.is>
2023-08-23 22:33:53 -0400

    stylize login page

--- client/LoginSignup.jsx
+++ client/LoginSignup.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import styled from 'styled-components';
-import { Button } from './components/Inputs.jsx';
+import { Button, Input } from './components/Inputs.jsx';
 import LogoAsset from './assets/logo.png'
 
 const OuterContainer = styled.div`
@@ -16,21 +16,25 @@ const Container = styled.div`
   border-radius: 10px;
   height: 650px;
   margin: auto;
-  width: 300px;
+  margin-top: 40px;
+  width: 400px;
 `;
 
 const Logo = styled.img`
   height: 250px;
-  width: 300px;
+  width: 400px;
   background-color: red;
+  border-bottom: 2px solid #000000;
+  border-radius: 10px 10px 0px 0px;
 `;
 const Tagline = styled.div`
+  color: #25559B;  
   font-size: 20px;
+  font-family: 'Quicksand', sans-serif;
   font-weight: bold;
-  margin: 10px;
-  text-align: center;
-  color: #25559B;
-  `;
+  margin: 34px 10px 10px 10px;
+  text-align: center;  
+`;
 
 const Form = styled.form`
   display: flex;
@@ -42,27 +46,45 @@ const Form = styled.form`
   boxing-sizing: border-box;
   width: inherit;
   height: 100px;
-  `;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 40px;
+  width: 100%;
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  display: flex;
+  font-size: 16px;
+  justify-content: center;
+  margin-top: 18px;
+`;
 
 const LoginSignUp = (props) => {
-  const {logIn} = props;
+  const {onLogin} = props;
   const [error, setError] = useState();
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+  const isDisabled = useMemo(() => {
+    return !username || !password;
+  }, [username, password]);
 
   const handleLogin = async (e) => {
     // attempt to login the user in
-    // const route = isLogin ? '/api/login' : '/api/signup';  // TODO: confirm the routes
     e.preventDefault();
     const response = await fetch('/api/login', { 
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ username: username, password: password }),
     });
     const data = await response.json();
-    logIn(true)
-    if (data.error) {
-      setError(data.error);
+    if (data.err) {
+      setError(data.err);
+    } else {
+      onLogin(true);
     }
   }
   
@@ -70,35 +92,34 @@ const LoginSignUp = (props) => {
     const response = await fetch('/api/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ username: username, 
+        password: password }),
     });
     const data = await response.json();
-    logIn(true);
-    if (data.error) {
-      setError(data.error);
+    if (data.err) {
+      setError(data.err);
+    } else {
+      onLogin(true);
     }
   }
-
-
 
   return (
     <OuterContainer>
       <Container>
         <Logo src={LogoAsset} />
         <div>
-          <Tagline>Keeping you organized since 2023</Tagline>
+          <Tagline>Keeping You Organized Since 2023</Tagline>
+          {error && <ErrorMessage><span>{error}, check your username and password.</span></ErrorMessage>}
           <>
             <Form>
-              <input value={username} type="text" placeholder="Username" />
-              <br />
-              <input value={password} type="password" placeholder="*******" />
+              <Input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username"  />
+              <Input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*******" />
             </Form>
-            <br />
-            <Button onClick={handleLogin}>Login</Button>
-            <Button onClick={handleSignup}>Sign Up</Button>
-        
-          </>
-          
+            <ButtonContainer>
+              <Button disabled={isDisabled} onClick={handleLogin} variant="primary">Login</Button>
+              <Button disabled={isDisabled} onClick={handleSignup} variant="secondary">Sign Up</Button>
+            </ButtonContainer>
+          </>          
         </div>
       </Container>
     </OuterContainer>
--- client/components/Inputs.jsx
+++ client/components/Inputs.jsx
@@ -7,6 +7,30 @@ Provide stylized inputs that can be resued throughout the site
 
 export const TextArea = styled.textarea``;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  border: lightgrey 1px solid;
+  border-radius: 5px;  
+  font-size: 16px;
+  height: 30px;
+  margin: 6px 0;
+  padding: 0 8px;
+  width: 250px;
+`;
 
-export const Button = styled.button``;
+export const Button = styled.button`
+  background-color: ${props => props.variant === "primary" ? "#51FDEF" : "#FFFCDC"};
+  border: #000030 ${props => props.variant === "primary" ? "0" : "1"}px solid;
+  border-radius: 10px;
+  color: #000030;
+  cursor: pointer;
+  font-size: 16px;  
+  height: 45px;
+  width: 90px;
+  &:hover {
+    background-color: ${props => props.variant === "primary" ? "#4BF0E3" : "#EEEABF"};
+  }
+  &:disabled {
+    background-color: ${props => props.variant === "primary" ? "#AFEDE7" : "#CDCBB1"};
+    cursor: not-allowed;
+  }
+`;
